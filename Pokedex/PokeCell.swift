c48b8561ede8299b59c2f175921af6d60f01c4a5
Frank <frank7300228@gmail.com>
2017-01-31 10:01:06 +0800

    version 1.0

--- Pokedex/PokeCell.swift
+++ Pokedex/PokeCell.swift
@@ -10,4 +10,20 @@ import UIKit
 
 class PokeCell: UICollectionViewCell {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var thumbImg: UIImageView!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
 }
